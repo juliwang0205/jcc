bf1c3f62cd9673cf13d700d541ce826c66966d0e
Juli Wang <wangjl66@chinatelecom.cn>
2024-02-27 18:08:16 +0800

    jcc: Improve error message
    Now, chibicc can print out an error message with an error location
    like this:
        $ ./jcc 1+jcc
        1+jcc
          ^ unexpected charater: j
    
    Signed-off-by : Juli Wang <wangjl66@chinatelecom.cn>

--- main.c
+++ main.c
@@ -21,6 +21,9 @@ struct Token {
     int len;        // Token length
 };
 
+// Input string
+static char *current_input;
+
 // Reports an error and exit.
 static void error(char *fmt, ...) {
     va_list ap;
@@ -28,6 +31,29 @@ static void error(char *fmt, ...) {
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);    
+}
+
+// Reports an error location and exit
+static void verror_at(char *loc, char *fmt, va_list ap) {
+    int pos = loc - current_input;
+    fprintf(stderr, "%s\n", current_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+static void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    verror_at(loc, fmt, ap);
+}
+
+static void error_tok(Token *tok, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    verror_at(tok->loc, fmt, ap);
 }
 
 // Consumes the current token if it matches 'op'
@@ -38,13 +64,13 @@ static bool equal(Token *tok, char *op){
 
 // Ensure that the current token is 's'
 static Token *skip(Token *tok, char *s){
-    if (!equal(tok, s)) error("expected '%s'", s);
+    if (!equal(tok, s)) error_tok(tok, "expected '%s'", s);
     return tok->next;
 }
 
 // Ensure that the curren token is TK_NUM
 static int get_number(Token *tok) {
-    if (tok->kind != TK_NUM) error("expected a number");
+    if (tok->kind != TK_NUM) error_tok(tok, "expected a number");
     return tok->val;
 }
 
@@ -57,8 +83,9 @@ static Token *new_token(TokenKind kind, char *start, char *end) {
     return tok;
 }
 
-// Tokenize 'p' and returns new tokens.
-static Token *tokenize(char *p) {
+// Tokenize 'current_input' and returns new tokens.
+static Token *tokenize(void) {
+    char *p = current_input;
     Token head = {};
     Token *cur = &head;
     while (*p) {
@@ -85,7 +112,7 @@ static Token *tokenize(char *p) {
         }
 
         // Error
-        error("unexpected charater: %c", *p);
+        error_at(p,"unexpected charater: %c", *p);
     }
 
     cur = cur->next = new_token(TK_EOF, p, p);
@@ -98,7 +125,8 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    Token *tok= tokenize(argv[1]);
+    current_input = argv[1];
+    Token *tok= tokenize();
 
     printf("   .global main\n");
     printf("main:\n");
